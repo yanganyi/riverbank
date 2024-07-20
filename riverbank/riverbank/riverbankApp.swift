08a19bba16d5a621f361bf50ede4240c4266a062
indigoX234 <parthasarathy_dhanush@s2023.ssts.edu.sg>
2024-07-20 14:19:05 +0800

    default
    nothing special yet

---
+++ riverbank/riverbank/riverbankApp.swift
@@ -0,0 +1,17 @@
+//
+//  riverbankApp.swift
+//  riverbank
+//
+//  Created by Dhanush  on 20/7/24.
+//
+
+import SwiftUI
+
+@main
+struct riverbankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
