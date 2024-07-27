340225bc934e43eb2079860ddff9c98051043971
timatolau <168737894+timatolau@users.noreply.github.com>
2024-07-27 16:31:53 +0800

    added pie chart

--- riverbank/riverbank/UsageView.swift
+++ riverbank/riverbank/UsageView.swift
@@ -9,9 +9,57 @@ import Foundation
 import SwiftUI
 
 struct UsageView: View {
+    @State var progress = -0.1
     var body: some View {
-        VStack {
-            Text("UsageView")
+
+        NavigationStack{
+            if progress != -0.1 {
+                ZStack {
+
+
+                    Circle()
+                        .stroke(
+                            Color.mint.opacity(0.5),
+                            lineWidth: 30
+                        )
+
+
+                    Circle()
+                        .trim(from: 0, to: progress)
+                        .stroke(
+                            Color.mint,
+                            style: StrokeStyle(
+                                lineWidth: 30,
+                                lineCap: .round
+                            )
+                        )
+                        .rotationEffect(.degrees(-90))
+                    // 1
+                        .animation(.easeOut, value: progress)
+
+
+                    // Transfer the data from HealthKit to the '100' part
+                    Text("Steps 100/2000")
+                        .font(.title2)
+                        .foregroundColor(Color.black)
+
+                }
+                .padding(20)
+                .navigationTitle("Home")
+
+            } else {
+                ProgressView()
+                    .onAppear(perform: {
+                        progress = 0.4
+
+
+                        // add HealthKit retreving data here
+
+                    })
+            }
         }
+
+      
+
     }
 }
