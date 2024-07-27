81309ac8eb631d299d1f5297c3369911407ba4fc
indigoX234 <parthasarathy_dhanush@s2023.ssts.edu.sg>
2024-07-27 16:25:56 +0800

    Added repeat and date fields

--- riverbank/riverbank/LogView.swift
+++ riverbank/riverbank/LogView.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct LogView: View {
+    
+    @State private var date = Date()
+    @State private var category = 0
+    @State private var repeated = 0
+    
+    var body: some View {
+        VStack {
+            List {
+                DatePicker(
+                    "Date",
+                    selection: $date,
+                    displayedComponents: [.date]
+                )
+                
+                Picker("Repeat", selection: $repeated) {
+                    Text("Never").tag(0)
+                    Text("Daily").tag(1)
+                    Text("Weekly").tag(2)
+                    Text("Monthly").tag(3)
+                }
+            }
+        }
+    }
+}
+
+#Preview{
+    LogView()
+}
