69c89beb3d5ed9a58109f16a537dbcb2e99bfb0a
yay <61081228+yanganyi@users.noreply.github.com>
2024-07-27 14:31:16 +0800

    build: create welcome screen

---
+++ riverbank/riverbank/WelcomeScreen.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeScreen.swift
+//  riverbank
+//
+//  Created by Yang An Yi on 27/7/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct WelcomeScreen: View {
+    var body: some View {
+        VStack {
+            Text("Welcome Screen")
+        }
+    }
+}
