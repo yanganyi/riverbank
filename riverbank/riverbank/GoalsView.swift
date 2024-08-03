47fff80dfc538c61c960cb367be7fbde967f5bf7
Aung Kaung Myat <akm292516@gmail.com>
2024-08-03 15:34:24 +0800

    hi

--- riverbank/riverbank/GoalsView.swift
+++ riverbank/riverbank/GoalsView.swift
@@ -8,11 +8,8 @@ struct GoalsView: View {
         "Cleaning",
         "Miscellaneous"
     ]
-    @State private var showAlert: Bool = false
-    @State private var alertMessage: String = ""
-    @State private var showInputAlert: Bool = false
     @State private var progressValues: [String: Double] = [
-        "Water Consumption": 0.5, // Example progress value
+        "Water Consumption": 0.5, // Example progress values
         "Shower": 0.3,
         "Cleaning": 0.8,
         "Miscellaneous": 0.1
@@ -31,72 +28,69 @@ struct GoalsView: View {
                 VStack(spacing: 20) {
                     ForEach(goals, id: \.self) { goal in
                         HStack {
-                            Text(goal)
-                                .padding()
-                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Button(action: {
+                                // Replace with desired action for the goal
+                                handleGoalButtonTap(goal)
+                            }) {
+                                HStack {
+                                    Text(goal)
+                                        .padding()
+                                        .frame(maxWidth: .infinity, alignment: .leading)
 
-                            VStack {
-                                GeometryReader { geometry in
-                                    let progressHeight = CGFloat(progressValues[goal] ?? 0.0) * geometry.size.height
                                     VStack {
-                                        Spacer()
-                                        Rectangle()
-                                            .fill(Color.blue)
-                                            .frame(width: 20, height: progressHeight)
+                                        GeometryReader { geometry in
+                                            let progressHeight = CGFloat(progressValues[goal] ?? 0.0) * geometry.size.height
+                                            VStack {
+                                                Spacer()
+                                                Rectangle()
+                                                    .fill(Color.blue)
+                                                    .frame(width: 20, height: progressHeight)
+                                            }
+                                        }
                                     }
+                                    .frame(width: 20, height: 60)
+                                    .background(Color.gray.opacity(0.3))
+                                    .cornerRadius(5)
+                                    .padding(.trailing)
                                 }
+                                .frame(height: 80)
+                                .background(Color.white)
+                                .cornerRadius(10)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color.blue, lineWidth: 2)
+                                )
                             }
-                            .frame(width: 20, height: 60) // Adjust the size of the progress bar
-                            .background(Color.gray.opacity(0.3)) // Background for the progress bar
-                            .cornerRadius(5)
-                            .padding(.trailing)
+                            .buttonStyle(PlainButtonStyle()) // Use plain style to avoid default button styling
                         }
-                        .frame(height: 80)
-                        .background(Color.white)
-                        .cornerRadius(10)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.blue, lineWidth: 2)
-                        )
                     }
                 }
                 .padding()
 
                 HStack {
+                    TextField("Enter new goal", text: $goalText, onCommit: {
+                        addGoal()
+                    })
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.blue, lineWidth: 2)
+                    )
+                    .frame(height: 40)
+
                     Button(action: {
-                        showInputAlert = true
+                        addGoal()
                     }) {
-                        Label("Add more goals", systemImage: "arrow.right.circle.fill")
-                            .font(.headline)
+                        Image(systemName: "plus.circle.fill")
+                            .font(.system(size: 24))
                             .foregroundColor(.blue)
-                            .frame(width: 290, height: 40)
-                            .padding()
-                            .background(Color.white)
-                            .cornerRadius(10)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color.blue, lineWidth: 2)
-                            )
+                            .padding(.trailing)
                     }
                 }
-                .padding(.top, 10)
                 .padding(.leading)
-
-                .alert(isPresented: $showAlert) {
-                    Alert(title: Text("Goal Added"),
-                          message: Text(alertMessage),
-                          dismissButton: .default(Text("OK")))
-                }
-                .alert(isPresented: $showInputAlert) {
-                    Alert(
-                        title: Text("New Goal"),
-                        message: Text("Enter your goal:"),
-                        primaryButton: .default(Text("Add")) {
-                            addGoal()
-                        },
-                        secondaryButton: .cancel()
-                    )
-                }
+                .padding(.trailing)
 
                 Spacer()
             }
@@ -105,12 +99,17 @@ struct GoalsView: View {
         }
     }
 
+    // Function to handle goal button tap
+    func handleGoalButtonTap(_ goal: String) {
+        print("\(goal) button tapped")
+        // Add your desired functionality here
+    }
+
+    // Function to add a new goal
     func addGoal() {
         guard !goalText.isEmpty else { return }
         goals.append(goalText)
         progressValues[goalText] = 0.0
-        alertMessage = "\(goalText) added!"
-        showAlert = true
         goalText = ""
     }
 }
